87cfca20aa7eeaa338b058ece018bc85a0691a12
Poupiio <elise.maaiza33@gmail.com>
2024-01-23 09:25:43 +0100

    Ajout swiper js

---
+++ index.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false
+    },
+});
