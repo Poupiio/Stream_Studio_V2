cf45992957abf84afc80af942eab0dec39ec779f
Poupiio <elise.maaiza33@gmail.com>
2024-01-23 16:19:05 +0100

    Modif flèche si écran > 800px

--- index.js
+++ index.js
@@ -14,12 +14,22 @@ var swiper = new Swiper(".mySwiper", {
 const images = document.querySelectorAll('.zoomIn');
 const windowWidth = window.innerWidth;
 
+
 images.forEach((img) => {
     img.addEventListener('click', () => {
         img.classList.toggle('zoom');
     })
 })
 
-if (windowWidth >= 992) {
-    console.log("ok");
+function arrowDirection() {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 800) {
+        changeArrow.innerHTML = "&#8594;";
+    } else {
+        changeArrow.innerHTML = "&#8595;";
+    }
 }
+
+window.addEventListener('resize', arrowDirection);
+window.addEventListener('load', arrowDirection);
